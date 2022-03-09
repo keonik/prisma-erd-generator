6ae17a7736c3ff83c0a50cc3ed854912728cf544
John Fay <jfay@miletwo.us>
2022-03-08 23:09:19 -0500

    test issue 66

---
+++ __tests__/issues/66.test.ts
@@ -0,0 +1,11 @@
+import * as child_process from 'child_process';
+
+test('many-to-many.prisma', async () => {
+    const fileName = '66.png';
+    const folderName = '__tests__';
+    child_process.execSync(`rm -f ${folderName}/${fileName}`);
+    child_process.execSync(`npx prisma generate --schema ./prisma/66.prisma`);
+    const listFile = child_process.execSync(`ls -la ${folderName}/${fileName}`);
+    // did it generate a file
+    expect(listFile.toString()).toContain(fileName);
+});
