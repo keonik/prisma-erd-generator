79280bcdf85b9551450ab80d54e7c839b08adb43
John Fay <jfay@miletwo.us>
2022-10-31 15:24:17 -0400

    chore(Dependency): Prisma 4.5 and mermaid cli 9.1.7

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@
 import { generatorHandler } from '@prisma/generator-helper';
 import generate from './generate';
 
+const disabled = Boolean(process.env.DISABLE_ERD);
+
 generatorHandler({
     onManifest: () => ({
-        defaultOutput: 'ERD.svg',
-        prettyName: 'Entity-relationship-diagram',
+        defaultOutput: disabled ? 'N/A' : 'ERD.svg',
+        prettyName: disabled ? 'No ERD' : 'Entity-relationship-diagram',
         requiresEngines: ['queryEngine'],
+        version: require('../package.json').version,
     }),
     onGenerate: generate,
 });
