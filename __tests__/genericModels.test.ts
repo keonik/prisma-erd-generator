67f95a8d58e3f1c433f1b0be479dcbdaf89453f8
Ghabi Anis <73026191+ghabianis@users.noreply.github.com>
2023-02-21 15:11:33 +0000

    first

--- __tests__/genericModels.test.ts
+++ __tests__/genericModels.test.ts
@@ -10,11 +10,7 @@ test('schema.prisma', async () => {
     const svgAsString = child_process.execSync(`cat ERD.svg`).toString();
     // did it generate a file with the correct content
     expect(svgAsString).toContain(`<svg`);
-    expect(svgAsString).toContain(`DefaultCalendar`);
-    expect(svgAsString).toContain(`users`);
-    expect(svgAsString).toContain(`Session`);
-    expect(svgAsString).toContain(`ConnectedCalendar`);
-    expect(svgAsString).toContain(`DailySchedule`);
-    expect(svgAsString).toContain(`Schedule`);
-    expect(svgAsString).toContain(`Booking`);
+    expect(svgAsString).toContain(`User`);
+    expect(svgAsString).toContain(`Store`);
+    expect(svgAsString).toContain(`Product`);
 });
